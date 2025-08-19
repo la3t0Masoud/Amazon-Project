be0074c1a98b055239d1fa2f047c6c26b610ad8e
la3t0Masoud <la3t.masoud11@gmail.com>
2025-08-19 17:27:53 +0330

    Initial commit

---
+++ data/cart.js
@@ -0,0 +1,89 @@
+export let cart = JSON.parse( localStorage.getItem(`Cart`)); 
+if(!cart){
+ cart =[
+    {
+    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 1,
+    deliveryOptionsId: `1`
+    },{
+    productId : `15b6fc6f-327a-4ec4-896f-486349e85a3d`,
+    quantity: 2,
+    deliveryOptionsId: `2`
+    }
+];
+}
+
+
+function SaveToStorage(){
+    localStorage.setItem(`Cart`,JSON.stringify(cart));
+}
+
+
+export function AddToCart(productId){
+
+    let matchingItem ;
+
+    cart.forEach((CartItem)=>{
+        if (productId === CartItem.productId){
+            matchingItem = CartItem;
+        }
+    });
+
+    if( matchingItem){
+        matchingItem.quantity += 1;
+    }else{
+        cart.push({
+        productId : productId,
+        quantity: 1,
+        deliveryOptionsId: `1`
+        });
+    }
+    SaveToStorage();
+}
+export function UpdateCartQuantity(){
+ let TotalQuantity = 0;
+    cart.forEach(product => {
+        TotalQuantity += product.quantity || 0 ;
+        document.querySelector(`.js-cart-quantity`).innerHTML = TotalQuantity;
+    });
+}
+
+
+export function RemoveFromCart(productId){
+
+    let newCart =[];
+
+    cart.forEach((CartItem) =>{
+
+        let Quantity = CartItem.quantity;
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        if(CartItem.productId === productId ){
+            if(Quantity > 1){
+                newCart.push(CartItem);
+               CartItem.quantity = CartItem.quantity-1 ;
+               document.querySelector(`.js-quantity-label-${CartItem.productId}`).innerHTML = CartItem.quantity;
+            }else if( Quantity === 1){
+            container.remove();
+            }
+        }else if( CartItem.productId !== productId ){
+            newCart.push(CartItem);
+        }
+        
+    });
+    cart = newCart;
+    SaveToStorage();
+}
+export function UpdateDeliveryOption(productId, deliveryOptionId){
+      let matchingItem ;
+
+    cart.forEach((CartItem)=>{
+        if (productId === CartItem.productId){
+            matchingItem = CartItem;
+        }
+    });
+    matchingItem.deliveryOptionsId = deliveryOptionId;
+
+    SaveToStorage();
+}
+
+        
---
+++ scripts/utils/money.js
@@ -0,0 +1,3 @@
+export function formatCurrencey(priceCents){
+    return(priceCents / 100).toFixed(2);
+}
