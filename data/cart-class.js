97a7f31c432a040de378faa9e9c9ad3dfa9ffa22
la3t0Masoud <la3t.masoud11@gmail.com>
2025-11-09 15:23:13 +0330

    make localStorageKey and loadFromStorage privete

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,15 @@
 class Cart{
     cartItems ;
-    localStorageKey ;
+    #localStorageKey ; // # --> privet property
+
 
     constructor(localStorageKey){
-        this.localStorageKey = localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey = localStorageKey;
+        this.#loadFromStorage();
     }
 
-    loadFromStorage(){
-        this.cartItems = JSON.parse( localStorage.getItem(this.localStorageKey)); 
+    #loadFromStorage(){
+        this.cartItems = JSON.parse( localStorage.getItem(this.#localStorageKey)); 
 
         if(!this.cartItems){
             this.cartItems =[
@@ -25,7 +26,7 @@ class Cart{
     };
 
     SaveToStorage(){
-        localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey,JSON.stringify(this.cartItems));
     }
 
     AddToCart(productId){
